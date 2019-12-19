e120933a9c30dd6d27ed95f5187a745a257a4bad
tejbahadur singh <tejbahadur.singh@nitorinfotech.com>
2019-12-19 20:20:05 +0530

    sample code to validate output of function

---
+++ tests/ValidateOutputTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase; 
+
+class ValidateOutputTest extends TestCase {
+    
+    public function testOutputStingSuccess(){
+        $objValidateOutput = new \source\ValidateOutput();
+        $this->assertNotEmpty($objValidateOutput->outputString());
+    }
+
+    public function testOutputStingFail(){
+        $objValidateOutput = new \source\ValidateOutput();
+        $this->assertEmpty($objValidateOutput->outputString());
+    }
+}
